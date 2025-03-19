ec6a618f541004f433a3707e5f9a26442bb26f77
Venu005 <venusaiyalamanchili@gmail.com>
2025-03-19 23:51:48 +0530

    chore:add dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config.js";
 import connectDB from "./config/db.js";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
